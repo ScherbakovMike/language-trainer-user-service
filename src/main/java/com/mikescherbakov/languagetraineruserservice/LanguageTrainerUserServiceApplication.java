82d2d0a217a781077ccfde995d03415d4ecba8f9
ScherbakovMike <scherbakov.mike@gmail.com>
2024-01-16 20:57:14 +0300

    Initial commit

---
+++ src/main/java/com/mikescherbakov/languagetraineruserservice/LanguageTrainerUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mikescherbakov.languagetraineruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageTrainerUserServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LanguageTrainerUserServiceApplication.class, args);
+  }
+
+}
