6c304f1819fecaa6a2169b17b55981e89540ea1f
ScherbakovMike <scherbakov.mike@gmail.com>
2024-01-16 20:57:15 +0300

    DynamoDbConfig class has been added.

---
+++ src/main/java/com/mikescherbakov/languagetraineruserservice/config/DynamoDbConfig.java
@@ -0,0 +1,11 @@
+package com.mikescherbakov.languagetraineruserservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class DynamoDbConfig {
+
+
+}
